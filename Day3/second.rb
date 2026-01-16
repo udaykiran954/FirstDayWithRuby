cdd536fccc54097ded872da6e65199db9c4e90bd
Mallela UdayKiran <mallelaudaykiran82@gmail.com>
2026-01-16 10:16:39 +0530

    Add files via upload

---
+++ Day3/second.rb
@@ -0,0 +1,31 @@
+class Product 
+    @@count = 1
+    def initialize(id,name,model)
+     @id = id
+     @name = name
+     @model = model
+    end
+
+    def get_all
+        return "[name: "+@name+ "\n id: #{@id}"+"\n model: "+@model+"]"
+    end
+
+    def set_count
+        @@count = @@count + 1
+    end
+    def get_count
+        @@count
+    end
+
+end
+
+p = Product.new(1,"car","benz");
+# puts Product.set_count
+# p1 = Product.new(2,"car","bmw");
+# puts Product.set_count
+# puts p1.get_all
+puts p.get_all
+p.set_count
+puts p.get_count
+
+
